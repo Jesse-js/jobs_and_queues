9d4757899dac1ff28e76c3a9728690a429a0c5cf
Jesse-js <jesse-jjs@hotmail.com>
2024-06-11 00:13:11 -0300

    feat: sends e-mail by jobs

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\PaymentJob;
 use App\Mail\PaymentMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -9,10 +10,8 @@ use Illuminate\Support\Facades\Mail;
 
 class PaymentController extends Controller
 {
-    public function payment(): void 
+    public function payment(): void
     {
-        //dd(Auth::user()->email);
-        // dd(new PaymentMail(Auth::user()));
-        Mail::to(Auth::user()->email, Auth::user()->name)->send(new PaymentMail(Auth::user()));
+        PaymentJob::dispatch(Auth::user());
     }
 }
---
+++ app/Jobs/PaymentJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\PaymentMail;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class PaymentJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(private User $user)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->user->email, $this->user->name)->send(new PaymentMail($this->user));
+    }
+}
