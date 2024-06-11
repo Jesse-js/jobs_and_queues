f251c2deea866eaa792a8eda58654a3aee8196bd
Jesse-js <jesse-jjs@hotmail.com>
2024-06-11 00:32:08 -0300

    feat: working with multiple jobs

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\PaymentJob;
+use App\Jobs\TestJob;
 use App\Mail\PaymentMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
-
 class PaymentController extends Controller
 {
     public function payment(): void
@@ -15,6 +15,7 @@ class PaymentController extends Controller
         // Mail::to(Auth::user()->email, Auth::user()->name)->send(new PaymentMail(Auth::user()));
         PaymentJob::dispatch(Auth::user())->delay(now()->addSeconds(5))
             ->onQueue('payments');
+        TestJob::dispatch();
         echo 'Enviado';
     }
 }
